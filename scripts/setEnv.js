b1886e16fd34c9a9adf6e9523b399a4aa9d5fa74
bitgadak <bitgadak@gmail.com>
2021-11-17 20:54:26 +0900

    기본 secretPrefix 추가

--- scripts/setEnv.js
+++ scripts/setEnv.js
@@ -1,7 +1,7 @@
-const { DefaultTokenKey, NodeEnv, ApiServer, ProductionEnv } = require('../config/env.json');
+const { DefaultSecretPrefix, DefaultTokenKey, NodeEnv, ApiServer, ProductionEnv } = require('../config/env.json');
 
 function setEnv({ isDev }) {
-  const [, scriptPath, production, tokenKeyArg] = process.argv;
+  const [, scriptPath, production, tokenKeyArg, secretPrefixArg] = process.argv;
 
   const productionEnv =
     ProductionEnv[production] ||
@@ -13,20 +13,27 @@ function setEnv({ isDev }) {
     process.env.TOKEN_KEY ||
     DefaultTokenKey;
 
+  const secretPrefix =
+    secretPrefixArg ||
+    process.env.SECRET_PREFIX ||
+    DefaultSecretPrefix;
+
   const [, scriptName] = scriptPath.match(/(\w+)\.js/i);
 
-  console.log(`[ 실행 명령어] npm run ${scriptName} ${productionEnv} ${tokenKey}`);
+  console.log(`[ 실행 명령어] npm run ${scriptName} ${productionEnv} ${tokenKey} ${secretPrefix}`);
 
   process.env.NODE_ENV = isDev ? NodeEnv['development'] : NodeEnv['production'];
   process.env.PRODUCTION_ENV = productionEnv;
   process.env.API_SERVER = isDev ? ApiServer['dev'] : ApiServer['real'];
   process.env.TOKEN_KEY = tokenKey;
+  process.env.SECRET_PREFIX = secretPrefix;
 
   console.log('[ 적용된 환경 변수 ]');
   console.log(`- process.env.NODE_ENV: ${process.env.NODE_ENV}`);
   console.log(`- process.env.PRODUCTION_ENV: ${process.env.PRODUCTION_ENV}`);
   console.log(`- process.env.API_SERVER: ${process.env.API_SERVER}`);
   console.log(`- process.env.TOKEN_KEY: ${process.env.TOKEN_KEY}`);
+  console.log(`- process.env.SECRET_PREFIX: ${process.env.SECRET_PREFIX}`);
 
   return { productionEnv };
 }
